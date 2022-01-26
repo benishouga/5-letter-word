c92405ae4187967654aa6ee8ecbf0dbae5cabf25
Akito Moriki <benishouga@gmail.com>
2022-01-27 02:22:02 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useMemo } from "react";
+import "./App.css";
+
+const MAX = 20;
 
 function App() {
+  const [words, setWords] = React.useState<string[]>([]);
+  const [regexp, setRegexp] = React.useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("./5-letter-word.txt");
+      const words = await res.text();
+      setWords(words.split("\n"));
+    })();
+  }, []);
+
+  const onRegexpChange = useCallback<
+    React.ChangeEventHandler<HTMLInputElement>
+  >((e) => setRegexp(e.target.value), [setRegexp]);
+
+  const filteredWords = useMemo(
+    () =>
+      words.filter((word) => {
+        if (regexp === "") return true;
+        try {
+          return new RegExp(regexp, "i").test(word);
+        } catch (e) {
+          return false;
+        }
+      }),
+    [words, regexp]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div className="header">
+        <div className="title">5 letter word</div>
+      </div>
+      <div className="body">
+        <p>This is a tool for those who are curious about 5-letter words.</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Data source:{" "}
+          <a href="https://github.com/dwyl/english-words">
+            https://github.com/dwyl/english-words
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          regexp:{" "}
+          <input
+            type="text"
+            style={{ width: "60%" }}
+            onChange={onRegexpChange}
+          />
+        </p>
+
+        <ul>
+          {filteredWords.slice(0, MAX).map((word, index) => {
+            const encoded = encodeURIComponent(word);
+            const alc = `https://eow.alc.co.jp/search?q=${encoded}`;
+            const phrase = `https://www.playphrase.me/#/search?q=${encoded}`;
+            const merriam = `https://www.merriam-webster.com/dictionary/${encoded}`;
+            return (
+              <li key={index}>
+                {word}{" "}
+                <small>
+                  <a target="_blank" rel="noreferrer" href={merriam}>
+                    merriam
+                  </a>{" "}
+                  <a target="_blank" rel="noreferrer" href={alc}>
+                    alc
+                  </a>{" "}
+                  <a target="_blank" rel="noreferrer" href={phrase}>
+                    phrase
+                  </a>
+                </small>
+              </li>
+            );
+          })}
+          {filteredWords.length > MAX ? (
+            <li>and more... {filteredWords.length - MAX} words.</li>
+          ) : null}
+        </ul>
+      </div>
     </div>
   );
 }
